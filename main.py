8236cb75e4452fa21f5b4d87ae8f7235ae11225b
Kacper Popek <monsyer888@gmail.com>
2026-02-21 14:33:41 -0500

    Add main.py and user funcktions

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
 import random
 from art import logo
 
+def deal_card():
+
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+def calculate_score(cards):
+ 
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+def compare(u_score, c_score):
+
+    if u_score == c_score:
+        return "Draw"
+    elif c_score == 0:
+        return "Lose, opponent has Blackjack"
+    elif u_score == 0:
+        return "Win with a Blackjack"
+    elif u_score > 21:
+        return "You went over. You lose"
+    elif c_score > 21:
+        return "Opponent went over. You win"
+    elif u_score > c_score:
+        return "You win"
+    else:
+        return "You lose"
